0df200c1594e5ec750b9ec6bc647221a35b013fa
Dangar-dev <danielsobraldev@gmail.com>
2025-08-07 14:22:21 -0300

    Criando a Controller Edicao

---
+++ Aula2projeto/Controllers/EdicaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula2projeto.Controllers
+{
+    public class EdicaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
