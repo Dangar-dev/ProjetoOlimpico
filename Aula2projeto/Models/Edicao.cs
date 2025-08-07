3b43d359d2320d99cf399786df88a49ee61bead7
Dangar-dev <danielsobraldev@gmail.com>
2025-08-07 14:35:26 -0300

    Criando a Model Edicao

---
+++ Aula2projeto/Models/Edicao.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula2projeto.Models
+{
+    public class Edicao
+    {
+        public int codEdicao { get; set; }
+
+        public int Ano { get; set; }
+
+        public string Sede { get; set; }
+
+    }
+}
