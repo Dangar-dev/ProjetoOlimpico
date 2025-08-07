7871dbae439232cc41ff443195e03eec952cca7a
3Amodular <3mod@bglab.local>
2025-08-07 14:12:29 -0300

    Criando o projetoOlimpico

---
+++ Aula2projeto/Data/DataBase.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+
+
+
+namespace Aula2projeto.Data
+{
+
+    
+        public class Database
+        {
+            private readonly string connectionString = "server=localhost;port=3306;database=dbOlimpicos;user=root;password=12345678;";
+
+            public MySqlConnection GetConnection()
+            {
+                MySqlConnection conn = new MySqlConnection(connectionString);
+                conn.Open();
+                return conn;
+            }
+        }
+ }
