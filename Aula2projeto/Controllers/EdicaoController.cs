9ca174fdb945f7f6c720e2a017f305ef0ba40d60
Dangar-dev <danielsobraldev@gmail.com>
2025-08-07 14:44:21 -0300

    Adicionadno o codigo da controller Edicao e criando a view Edicao Index

--- Aula2projeto/Controllers/EdicaoController.cs
+++ Aula2projeto/Controllers/EdicaoController.cs
@@ -1,11 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Aula2projeto.Data;
+using Aula2projeto.Models;
+using System.Collections.Generic;
+
 
 namespace Aula2projeto.Controllers
 {
     public class EdicaoController : Controller
     {
+        private readonly Database db = new Database();
         public IActionResult Index()
         {
+            List<Edicao> edicoes = new List<Edicao>();
+            using (MySqlConnection conn = db.GetConnection())
+            {
+                string sql = "SELECT * FROM tbEdicao";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        edicoes.Add(new Edicao
+                        {
+                            codEdicao = reader.GetInt32("codedicao"),
+                            Ano = reader.GetInt32("ano"),
+                            Sede = reader.GetString("sede")
+                        });
+
+
+                    }
+                }
+            }
             return View();
         }
     }
