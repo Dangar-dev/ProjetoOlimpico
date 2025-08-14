c3a9906db669aa0ce159eeea4e6712e73e49137f
Dangar-dev <danielsobraldev@gmail.com>
2025-08-14 16:53:33 -0300

    Criando a modalidade controller

---
+++ Aula2projeto/Controllers/ModalidadeController.cs
@@ -0,0 +1,37 @@
+﻿using Aula2projeto.Data;
+using Aula2projeto.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Aula2projeto.Data;
+
+namespace Aula2projeto.Controllers
+{
+    public class ModalidadeController : Controller
+    {
+        private readonly Database db = new Database();
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Modalidades modalidade)
+        {
+            using (var conn = db.GetConnection())
+            {
+                var sql = @"INSERT INTO tbModalidades (NomeModalidade) 
+
+                     VALUES (@nome)";
+
+                var cmd = new MySqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@nome", modalidade.nomeModalidade);
+
+                cmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index", "Edicao");
+        }
+
+    }
+}
---
+++ Aula2projeto/Controllers/ProvaController.cs
@@ -0,0 +1,69 @@
+﻿using Aula2projeto.Data;
+using Aula2projeto.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace Olimpiada.Controllers
+{
+    public class ProvaController : Controller
+    {
+        private readonly Database db = new Database();
+
+        public IActionResult Criar()
+        {
+            ViewBag.Modalidade = GetModalidade();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Prova prova)
+        {
+            using (var conn = db.GetConnection())
+            {
+                var sql = @"INSERT INTO tbProvas(nomeProva, codModalidade) 
+
+                     VALUES (@nome, @modalidade)";
+
+                var cmd = new MySqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@nome", prova.nomeProva);
+
+                cmd.Parameters.AddWithValue("@modalidade", prova.codModalidade);
+
+                cmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index", "Edicao");
+        }
+
+        private List<Modalidades> GetModalidade()
+        {
+            List<Modalidades> modalidades = new List<Modalidades>();
+            using (var conn = db.GetConnection())
+
+            {
+
+                var sql = "SELECT Distinct * FROM tbModalidades order by NomeModalidade";
+
+                var cmd = new MySqlCommand(sql, conn);
+
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+
+                    modalidades.Add(new Modalidades
+                    {
+
+                        codModalidade = reader.GetInt32("codModalidade"),
+
+                        nomeModalidade = reader.GetString("NomeModalidade"),
+
+                    });
+
+                }
+
+            }
+
+            return modalidades;
+        }
+    }
+}
