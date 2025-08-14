f65d5a78454c4ed7cc3e65227a48d6d0b6564762
Dangar-dev <danielsobraldev@gmail.com>
2025-08-14 16:59:15 -0300

    Criando as controllers restantes

---
+++ Aula2projeto/Controllers/Edicao.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula2projeto.Controllers
+{
+    public class Edicao : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
